e8ff0f91da196f1e79a94cc0e7db0237b8628b78
helys-l <dwiphelyas@gmail.com>
2025-04-01 20:42:21 +0700

    Update server.py

--- server.py
+++ server.py
@@ -32,5 +32,5 @@ def get_formats():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=10000)
