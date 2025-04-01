3ec5966af2a06eb68a8e39bbd019c5c3ea2f7177
helys-l <dwiphelyas@gmail.com>
2025-04-01 21:25:36 +0700

    Update server.py

--- server.py
+++ server.py
@@ -34,4 +34,4 @@ def get_formats():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host="0.0.0.0", port=10000)
